f1d6efb4f8812815ca2725dd18ded9fee623aaa2
Wei Li <wei.li@redhat.com>
2017-05-11 15:10:16 +0100

    🐛 make sure the right parameters are passed to the gauge constructor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var MetricsClients = require('./lib/clients');
+var types = require('./lib/types');
 
 module.exports = function metrics(conf) {
   //configure config module
@@ -12,7 +13,7 @@ module.exports = function metrics(conf) {
   return {
     "cpu": confEnabled ? require('./lib/cpu')(clients, baseTags) : nothing,
     "memory": confEnabled ? require('./lib/memory')(clients, baseTags) : nothing,
-    "gauge": confEnabled ? require('./lib/gauge')(clients, baseTags) : nothing,
+    "gauge": confEnabled ? require('./lib/gauge')(clients, types.G, baseTags) : nothing,
     "inc": confEnabled ? counter.inc : nothing,
     "dec": confEnabled ? counter.dec : nothing,
     "timer": confEnabled ? require('./lib/timer')(clients, baseTags) : nothing
